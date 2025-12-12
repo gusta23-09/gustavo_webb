0597210bc87cb8b4c544c8366ae9a469b0a9dbca
gusta23-09 <gustavoaraujooliveirarodrigues@gmail.com>
2025-12-12 14:22:48 -0300

    bomba patch

--- apps/aluno/forms.py
+++ apps/aluno/forms.py
@@ -1,4 +1,23 @@
+from django import forms
 from apps.aluno.models import Aluno
 
 class AlunoForms(forms.ModelForm):
-    pass
+    class Meta:
+        model = Aluno
+        exclude = ['',]
+        labels = {
+            'matricula' : 'Matricula',
+            'status' : 'Ativo'
+        }
+
+        widgets = {
+            'nome' : forms.TextInput(attrs={'class': 'form-control', 'autofocus': 'autofocus'}),
+
+            'matricula': forms.TextInput(attrs={'class': 'form-control'}),
+
+            'email': forms.TextInput(attrs={'class': 'form-control'}),
+
+            'telefone':forms.TextInput(attrs={'class': 'form-control'}),
+
+            'status': forms.CheckboxInput(attrs={'class': 'form-control' 'form-check-input'})
+        }
